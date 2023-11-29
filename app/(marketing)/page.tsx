5c7dd8ffb5f6a5d466574825989f0d86387de06b
Nyasha Musanhu <danielsnyasha@outlook.com>
2023-11-29 08:26:02 +0200

    commit

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <div className=''>
+      <Button className='bg-emerald-500' variant="destructive" size='lg'>
+        Click Me!
+        </Button>
+
+    </div>
+  )
+}
